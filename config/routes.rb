94e9e06bef1153433749a45288134895f58cb7a7
eimearcomerford <eimearmcomerford@gmail.com>
2016-08-20 15:49:05 +0100

    added new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
 
-  root 'item#index' 
-  get "item/:id" => "item#show", as: "item_show" 
+  
+
+  root "item#index"  
+  get "item/new" => "item#new", as: "item_new" 
+  post "item/new" => "item#create" 
+  get "item/:id" => "item#show", as: "item_show"
 
 
 end
