ffcbccf641f949dbabd30c5f5f4e1d3faf4b15ef
eimearcomerford <eimearmcomerford@gmail.com>
2016-08-21 12:00:56 +0100

    added edit items

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   
 
-  root "item#index"  
+  root "item#index"
   get "item/new" => "item#new", as: "item_new" 
-  post "item/new" => "item#create" 
+  post "item/new" => "item#create"
+  get "item/edit/:id" => "item#edit", as: "item_edit"
+  put "item/edit/:id" => "item#update"
   get "item/:id" => "item#show", as: "item_show"
 
 
